3b80bc664739420b82d1463a31ce48dad1b8c2b8
Allan Sham <allansham@hotmail.com>
2019-11-10 15:26:25 +0000

    testing

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">Hi, Louis and Gema TESTING.</div>;
+  return <div className="App">Hi, Louis and Gema.</div>;
 }
-//TESTING
 
 export default App;
