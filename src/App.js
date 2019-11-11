9707bf620153221c47d27f9bd87e5ea68b2579b6
subject026 <sudobuildspeakers@gmail.com>
2019-11-11 11:59:04 +0000

    test a commit on master

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">Hi, Louis and Gema.</div>;
+  return (
+    <div>
+      <h1>Hi, Allan and Gema.</h1>
+      <p>Master and development branches are now protected!</p>  
+    </div>
+  );
 }
 
 export default App;
