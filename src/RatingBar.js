04e568c7b73a4fc0c62533d054e7a7466f9f0d1d
Allan Sham <allansham@hotmail.com>
2019-11-12 16:07:46 +0000

    Add working rating bar

--- src/RatingBar.js
+++ src/RatingBar.js
@@ -1,8 +1,41 @@
 import React from "react";
 import styled from "styled-components";
 
+const OuterBar = styled.div`
+  width: 200px;
+  height: 10px;
+  border-radius: 10px;
+  background-color: #ebebeb;
+`;
+
+const InnerBar = styled.div`
+  height: 10px;
+  border-radius: 10px 0 0 10px;
+`;
+
 function RatingBar(props) {
-  return <a>{props.rating}</a>;
+  const setColor = rating => {
+    if (rating >= 6.6) {
+      return "MediumSeaGreen";
+    } else if (rating >= 3.3) {
+      return "Gold";
+    } else {
+      return "red";
+    }
+  };
+
+  const color = setColor(props.rating);
+
+  return (
+    <OuterBar>
+      <InnerBar
+        style={{
+          width: `${props.rating * 20}px`,
+          backgroundColor: `${color}`
+        }}
+      ></InnerBar>
+    </OuterBar>
+  );
 }
 
 export default RatingBar;
