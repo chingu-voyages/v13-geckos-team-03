7f00413001493d55f6719a6dc7e51f7ac215399e
gemasegarra <gema.segb@gmail.com>
2019-11-13 18:18:56 +0100

    Create footer.js

---
+++ src/Components/Footer/footer.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+
+const Footerbar = styled.nav`
+  display: flex;
+  justify-content: center;
+  align-items: center;  
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  height: 85px;
+  background: #F6F6F6;
+  font-size: 0.7rem;
+  color: #3B3272;
+  & a:visited {
+    color: #3B3272;
+  }
+  `
+
+class Footer extends Component {
+  render() {
+    return (
+      <Footerbar>
+        <p>Coded by <a href="https://github.com/chingu-voyages/v13-geckos-team-03"> Geckos Team 3</a> | Voyage 13, Chingu | 2019</p>
+      </Footerbar>)
+  }
+};
+
+export default Footer;
