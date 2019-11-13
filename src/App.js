8ed095b52608f7be6aba1e1bf3748164e4c904bc
Allan Sham <allansham@hotmail.com>
2019-11-13 20:16:05 +0000

    Add API call for testing (need to delete later)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import React from "react";
-import "./App.css";
+import React, { useState, useEffect } from "react";
+import Results from "./Results";
 
 function App() {
-  return <div className="App">Hi, Louis and Gema.</div>;
+  const [searchResults, setSearchResults] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch(
+        `https://api.themoviedb.org/3/search/movie?api_key=4c2694a90a597f3a2cf08d1ddcf2973e&query=Lion+King`
+      );
+      const data = await response.json();
+      const item = data.results;
+      setSearchResults(item);
+    }
+    fetchData();
+  }, []);
+
+  return (
+    <div>{searchResults && <Results searchResults={searchResults} />}</div>
+  );
 }
 
 export default App;
