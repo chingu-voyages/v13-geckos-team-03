22c9db03aad42898857e03848f99638ca4790643
Allan Sham <allansham@hotmail.com>
2019-11-14 21:05:11 +0000

    Remove API call code I used for testing.

--- src/App.js
+++ src/App.js
@@ -2,24 +2,7 @@ import React, { useState, useEffect } from "react";
 import Results from "./Results";
 
 function App() {
-  //BELOW for testing, remove later
-  const [searchResults, setSearchResults] = useState(null);
-  useEffect(() => {
-    async function fetchData() {
-      const response = await fetch(
-        `https://api.themoviedb.org/3/search/movie?api_key=4c2694a90a597f3a2cf08d1ddcf2973e&query=Lion+King`
-      );
-      const data = await response.json();
-      const item = data.results;
-      setSearchResults(item);
-    }
-    fetchData();
-  }, []);
-  //AFTER for testing, remove later
-
-  return (
-    <div>{searchResults && <Results searchResults={searchResults} />}</div>
-  );
+  return <div></div>;
 }
 
 export default App;
