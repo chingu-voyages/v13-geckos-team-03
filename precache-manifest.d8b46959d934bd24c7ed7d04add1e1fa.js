d5280adfa33a5062292a3545db8dd17dafc4e755
gemasegarra <gema.segb@gmail.com>
2019-11-19 22:41:39 +0100

    Updates

---
+++ precache-manifest.d8b46959d934bd24c7ed7d04add1e1fa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7634848978d524691a115995866301be",
+    "url": "/v13-geckos-team-03/index.html"
+  },
+  {
+    "revision": "b88d0c9eb9fb2eeded61",
+    "url": "/v13-geckos-team-03/static/js/2.bb7fa41a.chunk.js"
+  },
+  {
+    "revision": "c206720781a72db54387",
+    "url": "/v13-geckos-team-03/static/js/main.1b01a9f8.chunk.js"
+  },
+  {
+    "revision": "38c191b11d0efe824ce1",
+    "url": "/v13-geckos-team-03/static/js/runtime-main.569bff80.js"
+  },
+  {
+    "revision": "5f9fe8717cc0305dc1263a0fd40b0df7",
+    "url": "/v13-geckos-team-03/static/media/cog.5f9fe871.svg"
+  }
+]);
