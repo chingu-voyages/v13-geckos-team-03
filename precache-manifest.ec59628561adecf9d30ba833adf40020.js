879a2c9b4693890a82cf6fae81fdca647c2dceeb
gemasegarra <gema.segb@gmail.com>
2019-11-20 00:38:00 +0100

    Updates

--- precache-manifest.ec59628561adecf9d30ba833adf40020.js
+++ precache-manifest.ec59628561adecf9d30ba833adf40020.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7634848978d524691a115995866301be",
+    "revision": "9eb8b850c6c28e3c3b8c198e81423810",
     "url": "/v13-geckos-team-03/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/v13-geckos-team-03/static/js/2.bb7fa41a.chunk.js"
   },
   {
-    "revision": "c206720781a72db54387",
-    "url": "/v13-geckos-team-03/static/js/main.1b01a9f8.chunk.js"
+    "revision": "ef273a741a4264aa00b6",
+    "url": "/v13-geckos-team-03/static/js/main.89a34585.chunk.js"
   },
   {
     "revision": "38c191b11d0efe824ce1",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/v13-geckos-team-03/precache-manifest.d8b46959d934bd24c7ed7d04add1e1fa.js"
+  "/v13-geckos-team-03/precache-manifest.ec59628561adecf9d30ba833adf40020.js"
 );
 
 self.addEventListener('message', (event) => {
