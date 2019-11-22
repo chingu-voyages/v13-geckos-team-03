08cacda0c4a73e0de0f8d38e8c013ced7f93ee4d
subject026 <sudobuildspeakers@gmail.com>
2019-11-22 21:57:20 +0000

    supressed mongoose warning & rm console log

--- backend/DB.js
+++ backend/DB.js
@@ -3,10 +3,9 @@ const mongoose = require("mongoose");
 const config = require("./config")
 
 module.exports = async () => {
-  console.log(config.DB_URL);
   try {
-  await mongoose.connect(config.DB_URL, {useNewUrlParser: true})
-  console.log("The mongoose has landed");
+    await mongoose.connect(config.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+    console.log("The mongoose has landed");
   } catch (err) {
     console.log(err);
   };
