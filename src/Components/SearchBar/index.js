95218bda53b93bb073e41ac41855c6ac45ac98ac
Allan Sham <allansham@hotmail.com>
2019-11-23 21:11:20 +0000

    Fix bug of not loading last page of results

--- src/Components/SearchBar/index.js
+++ src/Components/SearchBar/index.js
@@ -39,7 +39,6 @@ const FormGroup = styled.div`
 
 const SearchBar = ({ updateResults }) => {
   const [searchText, setSearchText] = useState("");
-  const [atBottom, setAtBottom] = useState(false);
   const [totalPages, setTotalPages] = useState(1);
   const [currentPage, setCurrentPage] = useState(1);
 
@@ -55,8 +54,8 @@ const SearchBar = ({ updateResults }) => {
       currentPage
     );
     updateResults([...results]);
-    setTotalPages(total_pages);
-    setCurrentPage(prev => prev + 1);
+    setTotalPages(total_pages + 1);
+    setCurrentPage(1);
   };
 
   // add event listener to scroll, please see handleScroll function
@@ -71,24 +70,20 @@ const SearchBar = ({ updateResults }) => {
       window.innerHeight + document.documentElement.scrollTop ===
       document.documentElement.offsetHeight
     ) {
-      setAtBottom(true);
+      if (currentPage < totalPages) {
+        setCurrentPage(prev => prev + 1);
+      }
     }
   };
 
   useEffect(() => {
-    if (!atBottom) return;
-    setCurrentPage(prev => prev + 1);
+    if (currentPage >= totalPages) return;
     fetchMoreFilms();
-  }, [atBottom]);
+  }, [currentPage]);
 
   async function fetchMoreFilms() {
-    const { total_pages, results } = await searchFilmTitle(
-      searchText,
-      currentPage
-    );
-    console.log(results);
+    const { results } = await searchFilmTitle(searchText, currentPage);
     updateResults(prev => [...prev, ...results]);
-    setAtBottom(false);
   }
 
   return (
