e16c430db7c2119f71d5579672a23f4d9a35710c
Allan Sham <allansham@hotmail.com>
2019-11-23 20:20:55 +0000

    Load more results when scroll to bottom

--- src/Components/SearchBar/index.js
+++ src/Components/SearchBar/index.js
@@ -56,23 +56,40 @@ const SearchBar = ({ updateResults }) => {
     );
     updateResults([...results]);
     setTotalPages(total_pages);
+    setCurrentPage(prev => prev + 1);
   };
 
   // add event listener to scroll, please see handleScroll function
   useEffect(() => {
     window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
+  });
 
   // when user scroll to bottom set atBottom to true
   const handleScroll = () => {
     if (
-      window.innerHeight + document.documentElement.scrollTop >=
+      window.innerHeight + document.documentElement.scrollTop ===
       document.documentElement.offsetHeight
     ) {
       setAtBottom(true);
     }
   };
+
+  useEffect(() => {
+    if (!atBottom) return;
+    setCurrentPage(prev => prev + 1);
+    fetchMoreFilms();
+  }, [atBottom]);
+
+  async function fetchMoreFilms() {
+    const { total_pages, results } = await searchFilmTitle(
+      searchText,
+      currentPage
+    );
+    console.log(results);
+    updateResults(prev => [...prev, ...results]);
+    setAtBottom(false);
+  }
 
   return (
     <Container>
