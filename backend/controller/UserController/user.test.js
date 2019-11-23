00d3f482f71bdeaba7e51432bc111ceca2b26a62
subject026 <sudobuildspeakers@gmail.com>
2019-11-23 00:00:54 +0000

    added test supertest test

--- backend/controller/UserController/user.test.js
+++ backend/controller/UserController/user.test.js
@@ -1,3 +1,13 @@
-it("Test the tests", () => {
-    expect(1).toBe(1);
+require("dotenv").config();
+
+const app = require("../../app");
+const supertest = require("supertest");
+
+const request = supertest(app);
+
+describe("This is a describe call", () => {  
+    it("unmatched routes should return 404", async () => {
+        const res = await request.get("/api/cheese");
+        expect(res.status).toBe(404);
+    })
 })
