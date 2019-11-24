0fe3e4e2a423ec2cd04023e01c7fead325c53780
Allan Sham <allansham@hotmail.com>
2019-11-24 10:57:06 +0000

    Add Prettier formatting

--- src/HomePageView.js
+++ src/HomePageView.js
@@ -2,32 +2,33 @@ import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 
 import Results from "./Components/Results/Results";
-import APIKEY from './apikey.js';
+import APIKEY from "./apikey.js";
 
 const Title = styled.h1`
-    text-align: center;
-    font-size: 40px;
-    padding: 20px;`
+  text-align: center;
+  font-size: 40px;
+  padding: 20px;
+`;
 
+export default function() {
+  const [results, setResults] = useState([]);
+  const [] = useState(["popular"]);
 
-export default function () {
-    const [results, setResults] = useState([]);
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    useEffect(() => {
-        fetchData();
-    }, []);
-
-    const fetchData = async () => {
-        const res = await fetch(
-            `https://api.themoviedb.org/3/movie/popular?api_key=${APIKEY}&language=en-US&page=1`
-        );
-        const data = await res.json();
-        setResults(data.results);
-    }
-    return (
-        <div>
-            <Title>Popular films</Title>
-            {results.length === 0 ? null : <Results searchResults={results} />}
-        </div>)
-};
-
+  const fetchData = async () => {
+    const res = await fetch(
+      `https://api.themoviedb.org/3/movie/popular?api_key=${APIKEY}&language=en-US&page=1`
+    );
+    const data = await res.json();
+    setResults(data.results);
+  };
+  return (
+    <div>
+      <Title>Popular films</Title>
+      {results.length === 0 ? null : <Results searchResults={results} />}
+    </div>
+  );
+}
