a617a472886464b1d81243ee264be4e6f5d71dd6
subject026 <sudobuildspeakers@gmail.com>
2019-11-24 17:03:10 +0000

    removed model presave thing

--- backend/resources/auth/user.model.js
+++ backend/resources/auth/user.model.js
@@ -19,19 +19,4 @@ const userSchema = new mongoose.Schema(
   }
 );
 
-// userSchema.pre("save", function(next) {
-//   // don't hash password if it hasn't been changed
-//   // if(!this.isNew) return next();
-
-//   bcrypt.hash(this.password, 8)
-//     .then(function(hash) {
-//       this.password = hash;
-//       next();
-//     })
-//     .catch(err => {
-//       console.log(err);
-//       next();
-//     })
-// });
-
 module.exports = mongoose.model("user", userSchema);
