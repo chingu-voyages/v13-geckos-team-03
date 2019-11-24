612f92e33a38e11186f389abc07d0fa75255f560
subject026 <sudobuildspeakers@gmail.com>
2019-11-24 23:24:28 +0000

    added  focus animation for form labels

--- src/SignupView.js
+++ src/SignupView.js
@@ -1,15 +1,34 @@
 import React, { useState } from "react";
 
 import { signup } from "./Components/Network";
-import { Form } from "./StyledComponents/Forms";
+import { Form, FormLabel } from "./StyledComponents/Forms";
 
 export default function() {
   const [formState, updateFormState] = useState({
     email: "",
     password: "",
     confirmPassword: "",
+    hasFocus: "",
     errors: []
   });
+
+  const updateFocus = value => {
+    updateFormState(state => {
+      return {
+        ...state,
+        hasFocus: value
+      };
+    });
+  };
+
+  const handleBlur = () => {
+    updateFormState(state => {
+      return {
+        ...state,
+        hasFocus: ""
+      };
+    });
+  };
 
   const handleInputChange = event => {
     const { value, id } = event.target;
@@ -30,39 +49,59 @@ export default function() {
     });
   };
 
-  const { email, password, confirmPassword } = formState;
+  const { email, password, confirmPassword, hasFocus } = formState;
   return (
     <div>
       <Form onSubmit={handleSubmit}>
         <h3>sign up</h3>
         <div>
-          <label for="email">email</label>
+          <FormLabel
+            htmlFor="email"
+            hasFocus={hasFocus === "email" ? true : false}
+          >
+            email
+          </FormLabel>
           <input
             id="email"
             type="email"
             value={email}
             onChange={handleInputChange}
-            onFocus={handleFocus}
+            onBlur={handleBlur}
+            onFocus={() => updateFocus("email")}
             required
           ></input>
         </div>
         <div>
-          <label for="password">password</label>
+          <FormLabel
+            htmlFor="password"
+            hasFocus={hasFocus === "password" ? true : false}
+          >
+            password
+          </FormLabel>
           <input
             id="password"
             type="password"
             value={password}
             onChange={handleInputChange}
+            onBlur={handleBlur}
+            onFocus={() => updateFocus("password")}
             required
           ></input>
         </div>
         <div>
-          <label for="confirmPassword">confirm password</label>
+          <FormLabel
+            htmlFor="confirmPassword"
+            hasFocus={hasFocus === "confirmPassword" ? true : false}
+          >
+            confirm password
+          </FormLabel>
           <input
             id="confirmPassword"
             type="password"
             value={confirmPassword}
             onChange={handleInputChange}
+            onBlur={handleBlur}
+            onFocus={() => updateFocus("confirmPassword")}
             required
           ></input>
         </div>
--- src/StyledComponents/Forms.js
+++ src/StyledComponents/Forms.js
@@ -1,8 +1,19 @@
 import styled from "styled-components";
+import { css } from "styled-components";
 
 export const Form = styled.form`
   max-width: 700px;
   label {
     position: absolute;
   }
+`;
+
+export const FormLabel = styled.label`
+  ${({ hasFocus }) => {
+    if (hasFocus) {
+      return css`
+        transform: translateY(-10px);
+      `;
+    }
+  }}
 `;
