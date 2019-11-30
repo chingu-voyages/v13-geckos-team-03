f42c6c2dafd53cdb4da12cef10fdb97099b4ec15
Allan Sham <allansham@hotmail.com>
2019-11-30 17:55:16 +0000

    Add function to fetch data on individual films

--- src/MyFilmsView.js
+++ src/MyFilmsView.js
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from "react";
+
+import APIKEY from "./apikey.js";
 import { BACKEND_URL } from "./config";
 
 export default function() {
   const [userData, setUserData] = useState([]);
+  const [filmData, setFilmData] = useState({});
 
   useEffect(() => {
     const getUserData = async () => {
@@ -11,15 +14,28 @@ export default function() {
         credentials: "include"
       });
       const data = await res.json();
-      console.log(data);
-      setUserData(data);
+      setUserData(data.docs);
     };
     getUserData();
+  }, []);
+
+  useEffect(() => {
+    const getFilmData = async id => {
+      const res = await fetch(
+        `https://api.themoviedb.org/3/movie/${id}?api_key=${APIKEY}`
+      );
+      const data = await res.json();
+
+      setFilmData(data);
+      //console.log(filmData);
+    };
+    getFilmData(944);
   }, []);
 
   return (
     <div>
       <h1>My Films View</h1>
+      {filmData.original_title}
     </div>
   );
 }
