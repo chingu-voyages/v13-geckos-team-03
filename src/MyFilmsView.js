cfcfd9da1dda7e309b3567623b7215f88333cb58
Allan Sham <allansham@hotmail.com>
2019-11-30 17:02:52 +0000

    Request the user's favourited films

--- src/MyFilmsView.js
+++ src/MyFilmsView.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { BACKEND_URL } from "./config";
 
 export default function() {
-    return (
-        <div>
-            <h1>My Films View</h1>
-        </div>
-    )
+  const [userData, setUserData] = useState([]);
+
+  useEffect(() => {
+    const getUserData = async () => {
+      const res = await fetch(`${BACKEND_URL}/api/user-film-meta`, {
+        method: "GET",
+        credentials: "include"
+      });
+      const data = await res.json();
+      console.log(data);
+      setUserData(data);
+    };
+    getUserData();
+  }, []);
+
+  return (
+    <div>
+      <h1>My Films View</h1>
+    </div>
+  );
 }
