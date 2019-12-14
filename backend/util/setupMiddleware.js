bdd5fa2b00cdf59cf38f6566c3858ba89ce4ff8f
subject026 <sudobuildspeakers@gmail.com>
2019-12-14 13:10:43 +0000

    tweaked origin url

--- backend/util/setupMiddleware.js
+++ backend/util/setupMiddleware.js
@@ -27,7 +27,7 @@ function SetupMiddleware(app) {
       : process.env.PROD_FRONTEND_URL;
   app.use(
     cors({
-      origin: "https://chingu-voyages.github.io/v13-geckos-team-03",
+      origin: "chingu-voyages.github.io/v13-geckos-team-03",
       credentials: true
     })
   );
