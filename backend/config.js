376e0ae9163ec5acc857f36f2dee7739858dc23f
subject026 <sudobuildspeakers@gmail.com>
2019-12-14 02:10:26 +0000

    fixed db url

--- backend/config.js
+++ backend/config.js
@@ -6,6 +6,9 @@ switch (process.env.MODE) {
   case "development":
     config.DB_URL = process.env.DEV_DB_URL;
     break;
+  case "production":
+    config.DB_URL = process.env.PROD_DB_URL;
+    break;
   case "test":
     config.DB_URL = process.env.TEST_DB_URL;
     break;
